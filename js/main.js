1fc168a1a6ac256f4a8c673a084d3d421ca84647
Jason Lewis <thejasonslewis@gmail.com>
2015-10-26 13:43:12 -0600

    js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-function sizeVideoFrame(){
-    $(window).resize(function() {
-        if($(window).width() > 992){
-            //var headerHeight = $(".navigation-nav").outerHeight();
-            var footerHeight = $("#footer").outerHeight();
-            var phoneHeight = $(".phone-number").outerHeight();
-            var headFootHeight =   headerHeight + footerHeight + phoneHeight;
-            var iframeHeight = document.documentElement.clientHeight - headFootHeight - 50;
-            $("#background-photo").css("height", iframeHeight)
-        }
-    });
-
-};
-
-$(function () {
-    sizeVideoFrame();
-});
+//function sizeVideoFrame(){
+//    $(window).resize(function() {
+//        if($(window).width() > 992){
+//            //var headerHeight = $(".navigation-nav").outerHeight();
+//            var footerHeight = $("#footer").outerHeight();
+//            var phoneHeight = $(".phone-number").outerHeight();
+//            var headFootHeight =   headerHeight + footerHeight + phoneHeight;
+//            var iframeHeight = document.documentElement.clientHeight - headFootHeight - 50;
+//            $("#background-photo").css("height", iframeHeight)
+//        }
+//    });
+//
+//};
+//
+//$(function () {
+//    sizeVideoFrame();
+//});
 
 $(document).foundation();
 $(".find-table").click(function(){
